28df2979f56cb05c5c44b528a68bebf3f84bb37b
artiom miasovskii <artyomka-5.5@mail.ru>
2024-09-17 13:55:49 +0500

    item

---
+++ item.js
@@ -0,0 +1,17 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  const items = document.querySelectorAll('.footer__item');
+
+  items.forEach((item) => {
+    const h3 = item.querySelector('h3');
+    const p = item.querySelector('p');
+  
+    h3.addEventListener('click', () => {
+      p.style.display = p.style.display === 'block' ? 'none' : 'block';
+    });
+  });
+
+
+});
